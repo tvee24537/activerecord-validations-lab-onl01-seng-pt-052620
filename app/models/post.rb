ea8b220f5b01a72b42d8fabf0c484cf94f11c46b
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-10-12 18:53:46 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,16 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :clickbait?
+
+  CLICKERS = [
+    /Won't Believe/i, 
+    /Secret/i, 
+    /Top[0-9]*/i, 
+    /Guess/i
+  ]
   
   def clickbait?
     thing = false
