77e8cb0d301dd4f72e47246c29a0f48ae1f8a917
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-10-12 18:51:12 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ActiveRecord::Base
+  
+  def clickbait?
+    if CLICKERS.none? {|tite| tite.match title}
+    # if CLICKERS.none? {|tite| tite.match title}
+    #   errors.add(:title, "must be clickbait")
+    # end
+    thing = false
+    CLICKERS.each do |x|
+      if x.match title
+      thing = true
+      break
+      end
+    end
+    if thing != true 
+      errors.add(:title, "must be clickbait")
+    end
+  end
 end
