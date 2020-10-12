3843975b45ffbc3636ba4e27fb5ac85b11702ecf
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-10-12 18:52:54 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,12 @@
 class Post < ActiveRecord::Base
   
   def clickbait?
-    if CLICKERS.none? {|tite| tite.match title}
-    # if CLICKERS.none? {|tite| tite.match title}
-    #   errors.add(:title, "must be clickbait")
-    # end
     thing = false
     CLICKERS.each do |x|
       if x.match title
       thing = true
       break
-      end
+    end
     end
     if thing != true 
       errors.add(:title, "must be clickbait")
